c67725950dccff5df21daa8b7c6f6cf2682c31fb
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2025-04-20 22:02:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ.get("TELEGRAM_API")
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
+    API_HASH = "b1a1fc3dc52ccc91781f33522255a880"
+    BOT_TOKEN = "6107202849:AAFpDZWUp-H45LAm0G3_cQXJfWJP1715cto"
+    TELEGRAM_API = "11405252"
+    OWNER = "1881720028"
+    OWNER_USERNAME = "aarctixinc"
+    PASSWORD = "arctix123"
+    DATABASE_URL = "mongodb+srv://arctix1:arctix1@whatsapp-db.rlhncul.mongodb.net/?retryWrites=true&w=majority"
+    LOGCHANNEL = "-1002695771838"  # Add channel id as -100 + Actual ID
+    GDRIVE_FOLDER_ID = "root"
+    USER_SESSION_STRING = ""
     IS_PREMIUM = False
     MODES = ["video-video", "video-audio", "video-subtitle", "extract-streams"]
